b743df054bc21acf09d5bb4b243196e5ada1a664
Yuriy Aksenov <Yura290610@yandex.ru>
2017-10-25 18:03:07 +0300

    Добавьте файлы проекта.

---
+++ SortVisualisator/ApplicationViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortVisualisator
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        public string AnimationState { get; set; } = "Start";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+
+    enum AnimationState
+    {
+        Sort,
+        Pause,
+        Resume,
+        Shuffle
+    }
+}
+
